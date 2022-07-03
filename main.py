342edcb5e83c423476fc73076100dea4a6c62a58
Victor Erike <verike7@gmail.com>
2022-07-03 19:08:18 +0100

    Modified the entire code, same code but different context

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
-class Student:
-    def __init__ (self, name, age, tracks, score) :
-        self.name = name
-        self.age = age
-        self.tracks = tracks
-        self.score = score
-        
-    print("Details of new student: ")
 
+# class declaration 
+
+class Student: 
+
+    #Initializes the Student Object 
+    def __init__ (self, name, age, track, score) :
+        self.name = name
+        self.age = age 
+        self.track = track 
+        self.score = score 
+
+    #Function that changes the students name
     def change_name (self, new_name) :
         self.name = new_name
-        print("New Name: ", self.name)
+        print(f"Changed name to :{new_name}")
 
+    #Function that changes the students age 
     def change_age (self, new_age) :
+        try: #validate if the new age is an integer
+            new_age = int(new_age)
+        except:
+            print("Failed to change age, Input Valid DataType")
+            raise Exception
+
         self.age = new_age
-        print("New Age: ", self.age)
+        print(f"Changed age to : {new_age}")
 
-    def add_track (self, tracks) :
-        self.tracks = tracks
-        print("New Tracks: ", self.tracks)
-        
+    #Function that changes the students track
+    def change_track (self, new_track) :
+        self.track = new_track
+        print(f"Changed track to : {new_track}")
+
+    #Function that returns the students score
     def get_score (self) :
-        print("Score: ", self.score)
-    
-   
+        score = print(f"Score remains : {self.score}")
+        return score
 
-Bob = Student(name="Bob", age=26, tracks=["FE","BE"], score=99.99)
+Bob = Student (name = "Bobin", age = 35, track = ["FE","BE"], score = 95.6)
 
-Bob.change_name("Verike")
-Bob.change_age(21)
-Bob.add_track("FullStack")
+Bob.change_name ("Verike")
+Bob.change_age (22)
+Bob.change_track ("Full Stack/ DevOps")
 Bob.get_score()
